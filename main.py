e170c3fec706bcf195db9587d2564e0f447715d5
Kevin <kevingroot@outlook.com>
2023-01-09 11:18:17 +0100

    added other frequency files

--- main.py
+++ main.py
@@ -7,25 +7,35 @@ frequencyFileNumber = int(input("""
 After execution, there will be a text file generated with the corresponding name of the frequency file(s) with all the words that weren't in your known words list.
 
 Which frequency file would you like to use to compare with your known words?
-1. Narou (Not yet implemented)
-2. Netflix
-3. Novel 5k (Not yet implemented)
-4. Slife of Life (Not yet implemented)
-5. Shonen (Not yet implemented)
-6. Visual Novel (Not yet implemented)
+1. Narou (to be improved)
+2. Netflix (to be improved)
+3. Novel 5k (to be improved)
+4. Slife of Life (to be improved)
+5. Shonen (to be improved)
+6. Visual Novel (to be improved)
 7. JLPT
 8. Wanikani (Not yet implemented)
 9. Dialect (Not yet implemented)
 """))
 
 if frequencyFileNumber < 7:
-    stars = int(input("Up to how many stars do you want to analyze? "))
+    stars = int(input("Up to how many stars do you want to analyze? (e.g. \"4\") "))
 elif frequencyFileNumber == 7:
     level = int(input("Up to which level do you want to analyze? (e.g. \"3\") "))
 
 # fileNameFrequency = input("What is the file name of the frequency list? (e.g. \"Netflix.json\") ")
+if frequencyFileNumber == 1:
+    fileNameFrequency = "Narou.json"
 if frequencyFileNumber == 2:
     fileNameFrequency = "Netflix.json"
+if frequencyFileNumber == 3:
+    fileNameFrequency = "Novel 5k.json"
+if frequencyFileNumber == 4:
+    fileNameFrequency = "Slice of Life.json"
+if frequencyFileNumber == 5:
+    fileNameFrequency = "Shonen.json"
+if frequencyFileNumber == 6:
+    fileNameFrequency = "Visual Novel.json"
 elif frequencyFileNumber == 7:
     fileNameFrequency = "JLPT.json"
 
@@ -37,7 +47,7 @@ dataFrequency = json.load(codecs.open(fileNameFrequency, 'r', 'utf-8-sig'))
 with open(fileNameWords, 'r', encoding='UTF-8') as file:
     dataWords = json.load(file)
 
-if frequencyFileNumber == 2:
+if frequencyFileNumber < 7:
     netflix(dataWords, dataFrequency, stars)
 if frequencyFileNumber == 7:
     jlpt(dataWords, dataFrequency, level)
--- readme.md
+++ readme.md
@@ -1,13 +1,8 @@
+This python scripts calculates the percentage of words you know compared with the words in the specified frequency file.
 # Setup
-This python scripts asks for the name of you migaku known words file (when you export it as backup) and for the migaku Netflix frequency file which you can download from http://dicts.migaku.io/ja/frequency_lists/Netflix.json
-<br><br>
-Put the two .json files in the same directory as the .py file and run the .py file.<br><br>
-If you have a weak computer, open the .py file in a text editor and delete the line 68 and 69:
-``` 
-print("For lower than 5 stars: ", end="")
-frequencyCalculator((len(frequency) - 1), 60000)
-```
-Since it goes through almost the entire frequency list otherwise.
+Download this repository (code -> Download ZIP) and export your Migaku Known Words file to the directory where you extracted this repository AND rename it to "Words.json".<br>
+To run the script, run main.py
+<br>
 ## Tutorial on running the python file
 ### Step 1: Opening the command prompt
 One way to run the .py file is to open the command prompt in the directory where your files are, you can do this by clicking on the path in the file explorer and typing in "cmd" and then hitting enter.<br>
